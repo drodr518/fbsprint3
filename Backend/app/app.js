d9af04a1b5240e6d26447da1236327567d7921e3
Joao Guimaraes <jguim002@fiu.edu>
2019-09-09 23:54:30 -0400

    configured admin_auth, connect to database

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -4,19 +4,19 @@ const bodyParser = require('body-parser');
 var helmet = require('helmet');
 const errorHandler = require('./middleware/error-handler.middleware');
 
-/*
+
 const firebase = require('firebase-admin');
-const firebaseAccountConfig = require('../firebase-config.json');
+const firebaseAccountConfig = require('../learnfiu_db.json');
 firebase.initializeApp({
 	credential: firebase.credential.cert(firebaseAccountConfig),
-	databaseURL: 'https://geektext002.firebaseio.com'
+	databaseURL: 'https://learnfiu.firebaseio.com'
 });
 
 const passport = require('passport');
 require('./security/passport.strategy')(passport);
 
 app.use(passport.initialize());
-app.use(passport.session());*/
+app.use(passport.session());
 
 app.use(helmet());
 
