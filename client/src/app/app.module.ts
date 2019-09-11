d07867a03e8dd92a44563a245349fff0e2c1e6c4
Joao Guimaraes <jguim002@fiu.edu>
2019-09-10 20:22:35 -0400

    added basic navigation function to navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,15 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material';
 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
