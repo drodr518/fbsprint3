86574b6744a8aa610ea0581aafb3c8cf7109ef1f
Joao Guimaraes <jguim002@fiu.edu>
2019-09-14 09:32:22 -0400

    opens external or file resources in new tab

--- client/src/app/courses/course/modules/modules.component.ts
+++ client/src/app/courses/course/modules/modules.component.ts
@@ -18,9 +18,9 @@ export class ModulesComponent implements OnInit {
 
   modules : Module[] = [
     {id: '0', name: 'Module 1', resources: [
-      {title: 'Google', link: "https://www.google.com"} as Content,
-      {title: 'Microsoft', link: "https://www.microsoft.com"} as Content,
-      {title: 'Youtube', link: "https://www.youtube.com"} as Content,
+      {title: 'Google', link: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"} as Content,
+      {title: 'Microsoft', link: ""} as Content,
+      {title: 'Youtube', link: ""} as Content,
     ], },
     {id: '1', name: 'Module 2', resources: [
       {title: 'Google', url: "https://www.google.com"} as Content,
@@ -29,8 +29,15 @@ export class ModulesComponent implements OnInit {
     ], }
   ];
 
+  
+
   constructor() { }
 
+
+  openInNewTab(url) {
+    window.open(url, "_blank");
+  }
+  
   ngOnInit() {
   }
 
