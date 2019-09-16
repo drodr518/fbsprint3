796925484cce5a4b2fa5d3881d37821fcd92a037
Daniel Rodriguez <danny@dannyrodriguez.com>
2019-09-16 19:38:39 -0400

    Minor changes to help module

--- client/src/app/helppage/helppage-routing.module.ts
+++ client/src/app/helppage/helppage-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HelppageComponent} from "./helppage/helppage.component";
+import {HelppageComponent} from './helppage/helppage.component';
 
 
 const routes: Routes = [
--- client/src/app/helppage/helppage/helppage.component.ts
+++ client/src/app/helppage/helppage/helppage.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HelppageComponent implements OnInit {
 
   courses = [
-    {title: 'Danny Rodriguez', prof: "Phone Number"},
-    {title: 'Admin 2', prof: "XXX"},
-    {title: 'Admin 3', prof: "XXX"},
-    {title: 'Admin 4', prof: "XXX"},
-    {title: 'Admin 5', prof: "XXX"},
-  //admins = [
-  //  {name: 'Danny Rodriguez', phone: "(305)-439-1452", email: "drodr518@fiu.edu"},
+    {title: 'Danny Rodriguez', prof: 'Phone Number'},
+    {title: 'Admin 2', prof: 'XXX'},
+    {title: 'Admin 3', prof: 'XXX'},
+    {title: 'Admin 4', prof: 'XXX'},
+    {title: 'Admin 5', prof: 'XXX'}
+   // admins = [
+   //  {name: 'Danny Rodriguez', phone: "(305)-439-1452", email: "drodr518@fiu.edu"},
    // {name: 'Joao Guiramaes', prof: "(999)-999-9999", email: "xxxxxx@fiu.edu"},
-  ]
+  ];
   constructor() { }
 
   ngOnInit() {
