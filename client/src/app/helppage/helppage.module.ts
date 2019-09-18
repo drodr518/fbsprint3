e919fdb37cc19771cf83fd0d9fe14ac904d59695
Daniel Rodriguez <danny@dannyrodriguez.com>
2019-09-18 17:29:27 -0400

    Path Changes for navbar

--- client/src/app/helppage/helppage.module.ts
+++ client/src/app/helppage/helppage.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { HelppageRoutingModule } from './helppage-routing.module';
 import { HelppageComponent } from './helppage/helppage.component';
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
--- client/src/app/nav/nav-routing.module.ts
+++ client/src/app/nav/nav-routing.module.ts
@@ -7,6 +7,8 @@ const routes: Routes = [
         {path: 'courses', loadChildren: () => import('../courses/courses.module').then(mod => mod.CoursesModule)},
         {path: 'dashboard', loadChildren: () => import('../dashboard/dashboard.module').then(mod => mod.DashboardModule)},
         {path: 'helppage', loadChildren: () => import('../helppage/helppage.module').then(mod => mod.HelppageModule)},
+        {path: 'home', loadChildren: () => import('../home/home.module').then(mod => mod.HomeModule)},
+        {path: 'security', loadChildren: () => import('../security/security.module').then(mod => mod.SecurityModule)},
 
       ]}
 ];
