0ef25f6f2e1b5fae04e96791cfc2a5e7f7fc61b5
Daniel Rodriguez <danny@dannyrodriguez.com>
2019-09-20 20:50:16 -0400

    Formatted Home Page

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
   declarations: [HomepageComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class HomeModule { }
