1d373d7757ab83f931049293c04b26245772982d
Joao Guimaraes <jguim002@fiu.edu>
2019-09-22 02:28:43 -0400

    added editor components

---
+++ client/src/app/courses/course/modules/module-editor/module-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-module-editor',
+  templateUrl: './module-editor.component.html',
+  styleUrls: ['./module-editor.component.scss']
+})
+export class ModuleEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
