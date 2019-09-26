e730a37e3e001e648adc980eb27e4356b4749927
Joao Guimaraes <jguim002@fiu.edu>
2019-09-25 21:28:41 -0400

    not able to eget agnular editor to work out yet

--- client/src/app/courses/course/info/course-detail-editor/course-detail-editor.component.ts
+++ client/src/app/courses/course/info/course-detail-editor/course-detail-editor.component.ts
@@ -1,6 +1,8 @@
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit, Optional, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
 
 @Component({
   selector: 'app-course-detail-editor',
@@ -19,10 +21,28 @@ export class CourseDetailEditorComponent implements OnInit {
   ) {
     this.courseForm = this.formBuilder.group({
       title: [data.name, Validators.required],
-      instructor: [data.instructor, Validators.required],
+      instructor: ["option3", Validators.required],
       description: [data.description, Validators.required],
     });
    }
+
+
+   config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',   
+    defaultFontName: 'Arial',
+    customClasses: [
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ]
+  };
   
 
   ngOnInit() {
